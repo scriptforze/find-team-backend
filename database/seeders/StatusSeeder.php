dfd25d61e3f5f8d297074431ee1ddca538377970
Juan David Garcia <juancho637@gmail.com>
2022-11-09 11:34:36 -0500

    add logic and general statuses of application

---
+++ database/seeders/StatusSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Status;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class StatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('statuses')->insert(Status::STATUSES);
+    }
+}
